62e88648f3d2105feee9d24dc96326c34ac9df2c
Beloslavaaaaa <b.marinova@yandex.com>
2025-01-21 09:05:29 +0200

    CommIt_FixedBugs

--- scripts/search.js
+++ scripts/search.js
@@ -1,24 +1,34 @@
-document.addEventListener('DOMContentLoaded', function(){
-document.getElementById('search-btn').addEventListener('click',function(){
-    var title = document.getElementById('search').value;
-    if(title.trim()){
-        fetch.fetchMovie();
-        document.getElementById('details-page').style.display = 'block';
-    }else {
-        document.getElementById('details-page').style.display = 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("search-btn").addEventListener("click", function () {
+    let title = document.getElementById("search").value.trim();
+    if (title) {
+      fetchMovie(title);
+      document.getElementById("details-page").style.display = "block";
+    } else {
+      alert("test");
+      document.getElementById("details-page").style.display = "none";
     }
+  });
 });
-});
+const api_key = '2663a47da530c754207396eef5db5fb1';
 function fetchMovie(title) {
- fetch(`https://api.themoviedb.org/3/search/movie?api_key={2663a47da530c754207396eef5db5fb1}&amp;query=${title}`)
- .then(response => response.json())
- .then(data => {
-    document.getElementById('title').textContent = data.results[0].title;
-    document.getElementById('release_date').textContent = data.results[0].release_date;
-    document.getElementById('overview').textContent = data.results[0].overview;
-    document.getElementById('vote_average').textContent = data.results[0].vote_average;
-    document.getElementById('original_language').textContent = data.results[0].original_language;
-    document.getElementById('poster').textContent = data.results[0].poster;
- })
- .catch(error => console.error(error));
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&amp;query=${title}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+        const dataResult = data.results[0];
+      document.getElementById("title").textContent = dataResult.title;
+      document.getElementById("release_date").textContent =
+      dataResult.release_date;
+      document.getElementById("overview").textContent =
+      dataResult.overview;
+      document.getElementById("vote_average").textContent =
+      dataResult.vote_average;
+      document.getElementById("original_language").textContent =
+      dataResult.original_language;
+      document.getElementById("poster").src =
+        "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
+    })
+    .catch((error) => console.error(error));
 }
